baf9934120816c4dbcf3fcf655f058615d5d8f35
Maksim Kramer <mkramer@c1r1p5.42quebec.com>
2024-11-28 19:28:44 -0500

    update isGameOpen for back-forward buttons

--- django/static/frontend/games/pong/pong.js
+++ django/static/frontend/games/pong/pong.js
@@ -529,16 +529,18 @@ function closeGameWindow() {
 
 function isGameOpen() {
     var windowExist = document.getElementById('myWindowGame');
+    var windowExist1 = document.getElementById('myWindowgame');
+    console.log("using the function - isGameOpen");
 
-    if (windowExist)
+    if (windowExist || windowExist1)
         return(0);
     else
     {
-        closeGame();  
+        closeGame();
          return(1);
-    }
-       
+    }    
 }
+
 function isMenuTournamentFormOpen() {
     const menutournament = document.getElementById('tournamentForm'); // Sélectionner le menu
     if(menutournament)
